dd6189c77f1fd6e853fdc361a0b6f5a3cde0ab27
pintoinfant <pintoinfant5650@gmail.com>
2023-04-23 10:50:47 +0530

    make classname

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,8 +4,8 @@ import { Logo } from './Logo'
 export default function Loader() {
   return (
     <div className='flex items-center justify-center h-screen'>
-      <div class="border shadow rounded-md py-4 px-9 max-w-sm">
-        <div class="animate-pulse">
+      <div className="border shadow rounded-md py-4 px-9 max-w-sm">
+        <div className="animate-pulse">
           <Logo className="h-20 w-auto" />
         </div>
       </div>
