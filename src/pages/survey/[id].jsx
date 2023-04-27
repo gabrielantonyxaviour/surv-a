b9befb3c2457057b2f423c70c5a963aa5c94b0c7
pintoinfant <pintoinfant5650@gmail.com>
2023-04-27 14:12:30 +0530

    set ip for location

--- src/pages/survey/[id].jsx
+++ src/pages/survey/[id].jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from "axios"
+
 
 export default function view() {
+  const [location, setLocation] = useState(null)
+
+  useEffect(() => {
+    getCountry()
+  }, [])
+
+
+  const getCountry = async () => {
+    try {
+      const res = await axios.get('https://ipapi.co/json')
+      setLocation(res.data.country_name)
+    } catch {
+      setLocation('Unknown')
+    }
+  }
+
   return (
-    <div>view</div>
+    <div>{location}</div>
   )
 }
