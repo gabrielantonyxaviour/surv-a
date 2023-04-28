e9555ef3af20772c2c1dfaa528beed4db84d7d82
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2023-04-28 22:36:25 +0530

    🫠 Add Induvidual Analytics

--- src/pages/admin/[id].jsx
+++ src/pages/admin/[id].jsx
@@ -4,6 +4,8 @@ import Loading from '@/components/Loading'
 import Head from 'next/head'
 import Header from '@/components/app/AppHeader'
 import WordCloud from 'react-d3-cloud'
+import ReactECharts from 'echarts-for-react'
+import * as echarts from 'echarts'
 
 export default function view() {
   // const router = useRouter()
@@ -11,12 +13,8 @@ export default function view() {
   const supabase = useSupabaseClient()
   const session = useSession()
   const [surveyId, setSurveyId] = useState('')
-  const [survey, setSurvey] = useState([])
-
-  // [
-  //   { question: 'How are you?', answers: ['Hello', 'Hi', 'Hey'] }
-  // ]
-
+  const [survey, setSurvey] = useState({})
+  const [analytics, setAnalytics] = useState([])
   useEffect(() => {
     privateRoute()
   }, [session])
@@ -39,15 +37,15 @@ export default function view() {
     data.forEach(({ response_country, label }) => {
       if (score[response_country]) {
         if (label == '"POSITIVE"') {
-          score[response_country].postive += 1
+          score[response_country].positive += 1
         } else {
           score[response_country].negative += 1
         }
       } else {
         if (label == '"POSITIVE"') {
-          score[response_country] = { postive: 1, negative: 0 }
+          score[response_country] = { positive: 1, negative: 0 }
         } else {
-          score[response_country] = { postive: 0, negative: 1 }
+          score[response_country] = { positive: 0, negative: 1 }
         }
       }
     })
@@ -60,6 +58,7 @@ export default function view() {
   }
 
   const fetchSurvey = async () => {
+    console.log('Fetching....')
     const surveyId = window.location.href.split('/')[4]
     setSurveyId(surveyId)
     const { data: survey } = await supabase
@@ -73,29 +72,148 @@ export default function view() {
       `
       )
       .eq('id', surveyId)
-    console.log(survey)
-    // const countries = [{
-    //   India:{positive: 12,negative: 34}
-    // }]
-    // const data = []
-    // survey[0].questions.forEach((question) => {
-    //   const temp = getDistinctCountries(question.answers)
-    //   Object.keys(temp).forEach((country) => {
-    //     data.push({
-    //       value: country[positive],
-    //     itemStyle: {
-    //       color: '#50C878',
-    //     },
-    //     })
-    //   })
-    // })
+    if (survey.length == 0) {
+      setSurvey({})
+      return
+    }
+    const analyticsData = []
+    survey[0].questions.forEach((question) => {
+      const temp = getDistinctCountries(question.answers)
+      analyticsData.push(temp)
+    })
     setSurvey(survey[0])
-    // {
-    //   india:{positive: 23,negative: 32},
-    //   england:
-    // }
+    console.log(survey)
+    setAnalytics(analyticsData)
+    // console.log('Entire Analytics Data: ' + JSON.stringify(analyticsData))
+    // console.log('First Analytics Data: ' + JSON.stringify(analyticsData[0]))
+    // console.log(
+    //   'Only values: ' + JSON.stringify(Object.values(analyticsData[0]))
+    // )
   }
-  // "\"POSITIVE\""
+  // const y = [
+  //   {
+  //     India: { positive: 12, negative: 34 },
+  //   },
+  // ]
+
+  function getQuestionCountryOption(index, isOverall) {
+    return {
+      title: {
+        text: isOverall == true ? 'Overall' : 'Geo Analysis',
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          crossStyle: {
+            color: '#999',
+          },
+        },
+      },
+      toolbox: {
+        feature: {
+          dataView: { show: true, readOnly: false },
+          magicType: { show: true, type: ['line', 'bar'] },
+          restore: { show: true },
+          saveAsImage: { show: true },
+        },
+      },
+      legend: {
+        bottom: 10,
+        data: [
+          { name: 'Positive', itemStyle: { color: '#50C878' } },
+          { name: 'Negative', itemStyle: { color: '#D22B2B' } },
+        ],
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: isOverall == true ? ['Overall'] : Object.keys(analytics[index]),
+          axisPointer: {
+            type: 'shadow',
+          },
+        },
+      ],
+      yAxis: [
+        {
+          type: 'value',
+          name: 'Surveys',
+          min: 0,
+          max: 10,
+          interval: 2,
+        },
+      ],
+      series: [
+        {
+          name: 'Positive',
+          type: 'bar',
+          tooltip: {
+            valueFormatter: function (value) {
+              return value
+            },
+          },
+          data:
+            isOverall == true
+              ? [
+                  {
+                    value: Object.values(analytics[index])
+                      .map((item) => item.positive)
+                      .reduce(
+                        (accumulator, currentValue) =>
+                          accumulator + currentValue,
+                        0
+                      ),
+                    itemStyle: {
+                      color: '#50C878',
+                    },
+                  },
+                ]
+              : Object.values(analytics[index]).map((item) => {
+                  return {
+                    value: item.positive,
+                    itemStyle: {
+                      color: '#50C878',
+                    },
+                  }
+                }),
+        },
+        {
+          name: 'Negative',
+          type: 'bar',
+          tooltip: {
+            valueFormatter: function (value) {
+              return value
+            },
+          },
+          data:
+            isOverall == true
+              ? [
+                  {
+                    value: Object.values(analytics[index])
+                      .map((item) => item.negative)
+                      .reduce(
+                        (accumulator, currentValue) =>
+                          accumulator + currentValue,
+                        0
+                      ),
+                    itemStyle: {
+                      color: '#D22B2B',
+                    },
+                  },
+                ]
+              : Object.values(analytics[index]).map((item) => {
+                  return {
+                    value: item.negative,
+                    itemStyle: {
+                      color: '#D22B2B',
+                    },
+                  }
+                }),
+        },
+      ],
+    }
+  }
+
   const data = [
     { text: 'Hey', value: 1000 },
     { text: 'lol', value: 200 },
@@ -141,6 +259,31 @@ export default function view() {
         }}
       />
     )
+  // Array of countries
+  // Array of positives
+  // Array of negatives
+  //  const x={
+  //       survey_title:"Survey 1",
+  //       questions: [
+  //         {
+  //           question:"How are you?",
+  //           answers:[
+  //             {
+  //               answer:"Good",
+  //               label:"POSITIVE",
+  //               score:0.9,
+  //               response_country:"India"
+  //             },
+  //             {
+  //               answer:"Bad",
+  //               label:"NEGATIVE",
+  //               score:0.1,
+  //               response_country:"India"
+  //             },
+  //           ],
+  //         },
+  //       ]
+  //     }
 
   return (
     <>
@@ -154,46 +297,73 @@ export default function view() {
           style={{ height: `calc(100vh - 40px)` }}
         >
           <div className="grow">
-            <h2 className="ml-2  text-2xl font-bold text-indigo-900">
-              {survey.survey_title}
+            <div className="ml-2  rounded-xl bg-indigo-500 p-4 text-center text-2xl font-bold text-white">
+              <h1>{survey.survey_title}</h1>
+            </div>
+            <h2 className=" ml-2 mt-6 text-2xl font-bold text-indigo-900">
+              World Cloud Analysis
             </h2>
-
-            {/* <div className="my-4 mr-4 flex h-[400px] flex-col items-center justify-center rounded-xl border-2 bg-white p-6 text-2xl font-bold text-gray-600"> */}
-            <WordCloud data={data} height={300} onWordClick={handleWordClick} />
-            {/* </div> */}
+            <p className="text-md mb-4 ml-4 font-semibold text-gray-600">
+              Hint: Hover over a word to view analytics. Click to view all
+              responses
+            </p>
+            <div className="select-none">
+              <WordCloud
+                data={data}
+                height={300}
+                onWordClick={handleWordClick}
+                onWordMouseOver={() => {}}
+              />
+            </div>
             <h2 className="ml-2  text-2xl font-bold text-indigo-900">
-              Responses
+              Response Analysis
             </h2>
             <div className="my-4 mr-4 flex flex-col items-center justify-center rounded-xl bg-indigo-500 p-6 text-white">
-              {survey.questions.map((question, index) => {
-                return (
-                  <>
-                    <div className="w-full">
-                      <h2 className="ml-2 mt-4 font-semibold">
-                        {index + 1 + ' | ' + question.question}
-                      </h2>
-                      <div className="mb-4 grid grid-cols-3 gap-4">
-                        {question.answers.length > 0 ? (
-                          question.answers.map((answer) => {
-                            return (
-                              <input
-                                type="text"
-                                className="mt-2 rounded-xl bg-indigo-700 px-4 py-2 text-white"
-                                value={answer.answer}
-                                disabled
-                              />
-                            )
-                          })
-                        ) : (
-                          <div className="text-md ml-6 mt-2 font-semibold text-white">
-                            No responses yet
-                          </div>
-                        )}
+              {Object.keys(survey).length > 0 ? (
+                survey.questions.map((question, index) => {
+                  return (
+                    <>
+                      <div className="w-full">
+                        <h2 className="ml-2 mt-4 font-semibold">
+                          {index + 1 + ' | ' + question.question}
+                        </h2>
+                        <div className="px-auto my-3 flex justify-around rounded-2xl bg-white pt-4">
+                          <ReactECharts
+                            option={getQuestionCountryOption(index, false)}
+                            style={{ height: 300, width: 400 }}
+                          />
+                          <ReactECharts
+                            option={getQuestionCountryOption(index, true)}
+                            style={{ height: 300, width: 400 }}
+                          />
+                        </div>
+                        <div className="mb-4 grid grid-cols-3 gap-4">
+                          {question.answers.length > 0 ? (
+                            question.answers.map((answer) => {
+                              return (
+                                <input
+                                  type="text"
+                                  className="mt-2 rounded-xl bg-indigo-700 px-4 py-2 text-white"
+                                  value={answer.answer}
+                                  disabled
+                                />
+                              )
+                            })
+                          ) : (
+                            <div className="text-md ml-6 mt-2 font-semibold text-gray-300">
+                              No responses yet
+                            </div>
+                          )}
+                        </div>
                       </div>
-                    </div>
-                  </>
-                )
-              })}
+                    </>
+                  )
+                })
+              ) : (
+                <div className="text-2xl text-black">
+                  Session Expired. Log out and Log in back
+                </div>
+              )}
             </div>
           </div>
         </main>
